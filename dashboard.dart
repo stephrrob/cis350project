8cb7ebb65a622984b28b80e7ac7be6e79bd951c0
Stephen Robinson <123778165+stephrrob@users.noreply.github.com>
2023-12-11 23:45:41 -0500

    Update dashboard.dart

--- dashboard.dart
+++ dashboard.dart
@@ -2,21 +2,36 @@ import 'package:flutter/material.dart';
 import 'chore_list_item.dart';
 
 class Dashboard extends StatelessWidget {
+  final List<Map<String, String>> sampleChores = [
+    {
+      'choreName': 'Clean the kitchen',
+      'assignedTo': 'John',
+    },
+    {
+      'choreName': 'Take out the trash',
+      'assignedTo': 'Alice',
+    },
+    // Add more sample chores here
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Chore Manager'),
       ),
-      body: ListView(
-        children: <Widget>[
-          ChoreListItem(choreName: 'Clean the kitchen', assignedTo: 'John'),
-          // Add more ChoreListItem widgets for other chores
-        ],
+      body: ListView.builder(
+        itemCount: sampleChores.length,
+        itemBuilder: (context, index) {
+          final chore = sampleChores[index];
+          return ChoreListItem(
+            choreName: chore['choreName']!,
+            assignedTo: chore['assignedTo']!,
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          // Implement navigation to chore creation screen
           Navigator.pushNamed(context, '/chore/create');
         },
         child: Icon(Icons.add),
