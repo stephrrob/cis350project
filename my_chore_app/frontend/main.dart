40b0d99b8c34f0b32b003456dc4518ac88de5c2e
Stephen Robinson <123778165+stephrrob@users.noreply.github.com>
2023-12-12 18:35:56 -0500

    Update main.dart

--- my_chore_app/frontend/main.dart
+++ my_chore_app/frontend/main.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'chore_bloc.dart'; // Import your ChoreBloc
+
+void main() {
+  runApp(MyApp());
+}
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final ChoreBloc choreBloc = ChoreBloc(); // Initialize the ChoreBloc
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,21 +20,111 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
       routes: {
         '/': (context) => BlocProvider(
-              create: (context) => choreBloc,
+              create: (context) => ChoreBloc(), // Create the ChoreBloc
               child: Dashboard(),
             ),
         '/chore/create': (context) => BlocProvider.value(
-              value: choreBloc,
+              value: BlocProvider.of<ChoreBloc>(context), // Provide the existing ChoreBloc
               child: ChoreCreation(),
             ),
         // Add routes for other screens here
       },
     );
   }
+}
+
+// Define the ChoreBloc
+class ChoreBloc extends Bloc<ChoreEvent, ChoreState> {
+  ChoreBloc() : super(ChoreInitial());
 
   @override
-  void dispose() {
-    choreBloc.close(); // Close the ChoreBloc when the app is disposed
-    super.dispose();
+  Stream<ChoreState> mapEventToState(ChoreEvent event) async* {
+    if (event is LoadChores) {
+      // Handle loading chores from the backend
+      try {
+        // Make an HTTP request to fetch chores
+        // Replace with your actual backend API call
+        final chores = await fetchChoresFromBackend();
+        yield ChoreLoadSuccess(chores);
+      } catch (e) {
+        yield ChoreLoadFailure('Failed to load chores');
+      }
+    } else if (event is CreateChore) {
+      // Handle creating a new chore
+      try {
+        // Make an HTTP request to create a chore
+        // Replace with your actual backend API call
+        final createdChore = await createChoreInBackend(event.choreData);
+        yield ChoreCreateSuccess(createdChore);
+      } catch (e) {
+        yield ChoreCreateFailure('Failed to create chore');
+      }
+    }
+    // Add more event handling logic as needed
   }
+}
+
+// Define ChoreEvent and ChoreState classes
+abstract class ChoreEvent {}
+
+abstract class ChoreState {}
+
+class LoadChores extends ChoreEvent {}
+
+class CreateChore extends ChoreEvent {
+  final Map<String, dynamic> choreData;
+
+  CreateChore(this.choreData);
+}
+
+class ChoreInitial extends ChoreState {}
+
+class ChoreLoadSuccess extends ChoreState {
+  final List<Chore> chores;
+
+  ChoreLoadSuccess(this.chores);
+}
+
+class ChoreLoadFailure extends ChoreState {
+  final String error;
+
+  ChoreLoadFailure(this.error);
+}
+
+class ChoreCreateSuccess extends ChoreState {
+  final Chore createdChore;
+
+  ChoreCreateSuccess(this.createdChore);
+}
+
+class ChoreCreateFailure extends ChoreState {
+  final String error;
+
+  ChoreCreateFailure(this.error);
+}
+
+// Define the Chore model class
+class Chore {
+  final String id;
+  final String choreName;
+  final String assignedTo;
+  final bool completed;
+
+  Chore({
+    required this.id,
+    required this.choreName,
+    required this.assignedTo,
+    required this.completed,
+  });
+}
+
+// Define functions to interact with the backend
+Future<List<Chore>> fetchChoresFromBackend() async {
+  // Implement logic to fetch chores from the backend
+  // Replace with your actual implementation
+}
+
+Future<Chore> createChoreInBackend(Map<String, dynamic> choreData) async {
+  // Implement logic to create a chore in the backend
+  // Replace with your actual implementation
 }
